d26a964fd49bfef6db47cb74888e386555d5036d
Arnau <arnau@ersilia.io>
2025-08-27 12:01:03 +0200

    cloning fastsolv

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -1,39 +1,38 @@
 # imports
 import os
-import csv
 import sys
-from rdkit import Chem
-from rdkit.Chem.Descriptors import MolWt
+import pandas as pd
+from fastsolv import fastsolv
 
 # parse arguments
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
-# current file directory
-root = os.path.dirname(os.path.abspath(__file__))
+# # current file directory
+# root = os.path.dirname(os.path.abspath(__file__))
 
-# my model
-def my_model(smiles_list):
-    return [MolWt(Chem.MolFromSmiles(smi)) for smi in smiles_list]
+# # my model
+# def my_model(smiles_list):
+#     return [MolWt(Chem.MolFromSmiles(smi)) for smi in smiles_list]
 
 
-# read SMILES from .csv file, assuming one column with header
-with open(input_file, "r") as f:
-    reader = csv.reader(f)
-    next(reader)  # skip header
-    smiles_list = [r[0] for r in reader]
+# # read SMILES from .csv file, assuming one column with header
+# with open(input_file, "r") as f:
+#     reader = csv.reader(f)
+#     next(reader)  # skip header
+#     smiles_list = [r[0] for r in reader]
 
-# run model
-outputs = my_model(smiles_list)
+# # run model
+# outputs = my_model(smiles_list)
 
-#check input and output have the same lenght
-input_len = len(smiles_list)
-output_len = len(outputs)
-assert input_len == output_len
+# #check input and output have the same lenght
+# input_len = len(smiles_list)
+# output_len = len(outputs)
+# assert input_len == output_len
 
-# write output in a .csv file
-with open(output_file, "w") as f:
-    writer = csv.writer(f)
-    writer.writerow(["value"])  # header
-    for o in outputs:
-        writer.writerow([o])
+# # write output in a .csv file
+# with open(output_file, "w") as f:
+#     writer = csv.writer(f)
+#     writer.writerow(["value"])  # header
+#     for o in outputs:
+#         writer.writerow([o])
